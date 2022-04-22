dbc1460cfa09556680f2c788fc9125fa839d1cde
Johannes Giorgis <johannes.giorgis@wegalvanize.com>
2022-04-22 15:29:35 -0700

    Get reading from dynamodb table working

--- url-shortener-app/lib/url-shortener-app-stack.ts
+++ url-shortener-app/lib/url-shortener-app-stack.ts
@@ -1,25 +1,53 @@
 import { aws_lambda, CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import {
+    LambdaToDynamoDB,
+    LambdaToDynamoDBProps,
+} from "@aws-solutions-constructs/aws-lambda-dynamodb";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class UrlShortenerAppStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
         const backend = new NodejsFunction(this, "handler", {
-            entry: "lambda/url-shortener.ts",
+            entry: "lambda/index.ts",
             handler: "handler",
             runtime: aws_lambda.Runtime.NODEJS_14_X,
         });
 
-        const fnUrl = backend.addFunctionUrl({
+        // Construct
+        const lambdaDynamoDBProps: LambdaToDynamoDBProps = {
+            existingLambdaObj: backend,
+            tableEnvironmentVariableName: "TABLE_NAME",
+        };
+
+        const ldConstruct = new LambdaToDynamoDB(
+            this,
+            "LambdaToDynamoDB",
+            lambdaDynamoDBProps
+        );
+
+        // Function URL
+        const fnUrl = ldConstruct.lambdaFunction.addFunctionUrl({
             authType: aws_lambda.FunctionUrlAuthType.NONE,
         });
+
+        ldConstruct.dynamoTable.grantReadWriteData(backend);
 
         new CfnOutput(this, "ShortenerUrl", {
             value: fnUrl.url,
             description: "Shortener URL",
+        });
+
+        new CfnOutput(this, "DynamoDBTable", {
+            value: ldConstruct.dynamoTable.tableName,
+            description: "DyanmoDB Table",
+        });
+
+        new CfnOutput(this, "LambdaFunction", {
+            value: ldConstruct.lambdaFunction.functionName,
+            description: "Lambda Function",
         });
     }
 }
